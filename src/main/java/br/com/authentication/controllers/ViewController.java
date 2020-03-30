c9fd7f7e90c69dc0cf44de06d0bbec387de0c3da
c82957a <c82957a@NB66224.serasa.intranet>
2020-03-30 11:20:44 -0300

    .

--- src/main/java/br/com/authentication/controllers/ViewController.java
+++ src/main/java/br/com/authentication/controllers/ViewController.java
@@ -1,8 +1,9 @@
 package br.com.authentication.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@org.springframework.stereotype.Controller
+@Controller
 public class ViewController {
 
 	@GetMapping("/login")
